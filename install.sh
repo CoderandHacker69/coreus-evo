3be3035b5fd0c554bc5b2d38bdded66362567830
n00dle <alexis.boisse35+1234@gmail.com>
2026-04-17 11:46:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ echo "Welcome to coreus installation process"
 echo "======================================"
 echo "Make sure to have npm, git and a systemd based linux system"
 echo "Step 1: Clone repo (may take some time)"
-git clone https://github.com/noodlelover1/coreus.git .coreus
-cd .coreus
+git clone https://github.com/noodlelover1/coreus.git /etc/coreus
+cd /etc/coreus
 echo "Step 2: Install dependencies"
 npm i
 echo "Step 3: Make binary executable"
